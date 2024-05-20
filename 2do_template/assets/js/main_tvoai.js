68b168dd7045e46ee2a2635b9baa7609b2e02ba2
Wuicho <130885313+wuiWuicho33@users.noreply.github.com>
2024-05-20 15:21:53 -0600

    Se agrega el swiper correcto

--- 2do_template/assets/js/main_tvoai.js
+++ 2do_template/assets/js/main_tvoai.js
@@ -44,3 +44,25 @@ window.onload = function() {
 Swiper carousel
 ====================*/ 
 // Initialize Swiper
+var swiper = new Swiper(".Swiper01", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+});
