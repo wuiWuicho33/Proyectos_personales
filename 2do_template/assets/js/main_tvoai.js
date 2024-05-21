b7f540704dfa5bd2549d88abcc5fae87311a9e52
Wuicho <130885313+wuiWuicho33@users.noreply.github.com>
2024-05-21 13:02:52 -0600

    Se agrega el back to top

--- 2do_template/assets/js/main_tvoai.js
+++ 2do_template/assets/js/main_tvoai.js
@@ -40,6 +40,38 @@ window.onload = function() {
     defaultTabButton.click();
 }
 
+
+/*====================
+Back to top
+====================*/ 
+// Mostrar u ocultar el botón según la posición del scroll
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("back-to-top").style.display = "block";
+  } else {
+    document.getElementById("back-to-top").style.display = "none";
+  }
+}
+
+// Desplazamiento suave al hacer clic en el botón
+document.getElementById("back-to-top").addEventListener("click", function(event) {
+  event.preventDefault();
+  scrollToTop(500); // 1000ms = 1 segundo
+});
+
+function scrollToTop(scrollDuration) {
+  const scrollStep = -window.scrollY / (scrollDuration / 15);
+  const scrollInterval = setInterval(function() {
+    if (window.scrollY !== 0) {
+      window.scrollBy(0, scrollStep);
+    } else {
+      clearInterval(scrollInterval);
+    }
+  }, 15);
+}
+
 /*====================
 Swiper carousel
 ====================*/ 
