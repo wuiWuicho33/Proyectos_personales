0d93fc8b8f842a793862d90cbafa5451f069060a
Wuicho <130885313+wuiWuicho33@users.noreply.github.com>
2024-05-24 11:42:54 -0600

    Se soluciona el problema del back to top

--- 2do_template/assets/js/main_tvoai.js
+++ 2do_template/assets/js/main_tvoai.js
@@ -41,37 +41,6 @@ window.onload = function() {
 }
 
 /*====================
-Back to top
-====================*/ 
-// Mostrar u ocultar el botón según la posición del scroll
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("back-to-top").style.display = "block";
-  } else {
-    document.getElementById("back-to-top").style.display = "none";
-  }
-}
-
-// Desplazamiento suave al hacer clic en el botón
-document.getElementById("back-to-top").addEventListener("click", function(event) {
-  event.preventDefault();
-  scrollToTop(500); // 1000ms = 1 segundo
-});
-
-function scrollToTop(scrollDuration) {
-  const scrollStep = -window.scrollY / (scrollDuration / 15);
-  const scrollInterval = setInterval(function() {
-    if (window.scrollY !== 0) {
-      window.scrollBy(0, scrollStep);
-    } else {
-      clearInterval(scrollInterval);
-    }
-  }, 15);
-}
-
-/*====================
 Swiper carousel
 ====================*/ 
 // Per-view-02
@@ -178,16 +147,45 @@ Main Nav
 
 })( window, document, jQuery );
 
+/*===========================
+Back to top & Sticky function
+===========================*/ 
+window.onscroll = function() {
+    scrollAndStickyFunction();
+};
 
-/*Sticky Menu*/
-window.onscroll = function() {myFunction()};
 var navbar = document.getElementById("menuSticky");
 var sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+function scrollAndStickyFunction() {
+    // Botón "Volver arriba"
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("back-to-top").style.display = "block";
+    } else {
+        document.getElementById("back-to-top").style.display = "none";
+    }
+
+    // Menú pegajoso
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
+// Desplazamiento suave al hacer clic en el botón "Volver arriba"
+document.getElementById("back-to-top").addEventListener("click", function(event) {
+    event.preventDefault();
+    scrollToTop(500); // 1000ms = 1 segundo
+});
+
+function scrollToTop(scrollDuration) {
+    const scrollStep = -window.scrollY / (scrollDuration / 15);
+    const scrollInterval = setInterval(function() {
+        if (window.scrollY !== 0) {
+            window.scrollBy(0, scrollStep);
+        } else {
+            clearInterval(scrollInterval);
+        }
+    }, 15);
 }
